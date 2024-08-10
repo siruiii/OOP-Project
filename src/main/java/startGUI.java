00607b3f96f7d3e52c88ecf18d481613e772037e
kl3267 <34148567-kl3267@users.noreply.replit.com>
2024-08-10 09:20:47 +0000

    Let Main be the run part

--- src/main/java/startGUI.java
+++ src/main/java/startGUI.java
@@ -13,22 +13,6 @@ public class startGUI extends JFrame {
   private JPanel contentPane;
 
   /**
-   * Launch the application.
-   */
-  public static void main(String[] args) {
-    EventQueue.invokeLater(new Runnable() {
-      public void run() {
-        try {
-          startGUI frame = new startGUI();
-          frame.setVisible(true);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      }
-    });
-  }
-
-  /**
    * Create the frame.
    */
   public startGUI() {
